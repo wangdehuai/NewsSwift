82c9c25cd82be8c97a26129460b8e4568c3392ff
wangdehuai <wangdehuai@github.com>
2019-07-12 15:50:34 +0800

    add

--- NewsSwift/NewsListTableViewController.swift
+++ NewsSwift/NewsListTableViewController.swift
@@ -9,72 +9,89 @@
 import UIKit
 
 class NewsListTableViewController: UITableViewController {
-
+    
     @IBOutlet weak var ImageView: UIImageView!
     @IBOutlet weak var Label: UILabel!
     let APIURL = "http://qingbin.sinaapp.com/api/lists?ntype=%E5%9B%BE%E7%89%87&pageNo-1&pagePer=10&list.htm"
-    let DataSource = []
+    let DataSource = [String]()
+    
+    func POSTWithURLSession(_ urlString:String,parmas:[String:Any],mathFunction:@escaping (_ responObject:AnyObject)->Void){
+        let session = URLSession.shared
+        let str = self.parmasStringWithParmas(parmas)
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.httpBody = str.data(using: .utf8)
+        let task = session.dataTask(with:request, completionHandler: { (data, respons, error) -> Void in
+            if data != nil{
+                let responsobject = try?JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
+                if responsobject == nil{
+                    mathFunction("没有内容" as AnyObject)
+                }else{
+                    mathFunction(responsobject! as AnyObject)
+                }
+            }else{
+                mathFunction("file" as AnyObject)
+            }
+        })
+        task.resume()
+    }
+    
+    func GETWithURLSession(_ urlString:String,parmas:[String:Any],mathFunction:@escaping (_ responObject:AnyObject)->Void){
+        let session = URLSession.shared
+        let str = self.parmasStringWithParmas(parmas)
+        let url = URL(string: "\(urlString)?\(str)")
+        let task = session.dataTask(with: url!, completionHandler: { (data, respons, eror) -> Void in
+            if data != nil{
+                let responsobject = try?JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                if responsobject == nil{
+                    mathFunction("没有内容" as AnyObject)
+                }else{
+                    mathFunction(responsobject! as AnyObject)
+                }
+            }else{
+                mathFunction("file" as AnyObject)
+            }
+        })
+        task.resume()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // self.clearsSelectionOnViewWillAppear = false
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        loadDataSource()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cell")
+        
+        POSTWithURLSession(APIURL, parmas: [:]) { (AnyObject) in
+            print(AnyObject)
+        }
     }
     
-    func loadDataSource(){
-        let url = NSURL(string: APIURL)
-        let request = NSURLRequest(URL: url!)
-        let LoadQueue = NSOperationQueue()
-        NSURLConnection.sendAsynchronousRequest(request, queue: LoadQueue) { (response, data, error) -> Void in
-            let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
-            let NewsDataSource = json["item"] as NSArray
-            var CurrentNewsData = NSMutableArray()
-            for currentnews in NewsDataSource{
-                let newsitem = NewsItem()
-                newsitem.NewsTitle = currentnews["title"] as NSString
-                newsitem.NewsID = currentnews["id"] as NSString
-                newsitem.NewsThumb = currentnews["thumb"] as NSString
-                
-                CurrentNewsData.addObject(newsitem)
-            }
-//            self.DataSource = CurrentNewsData
-            self.tableView.reloadData()
+    func parmasStringWithParmas(_ parmas:[String:Any])->String{
+        var parString = String()
+        for (key, value) in parmas{
+            parString.append(contentsOf: "\(key)=\(value)")
         }
-
+        return parString
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return DataSource.count
     }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "reusingCell")
-//        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-        cell.textLabel?.text = DataSource[indexPath.row].NewsTitle
-        cell.detailTextLabel?.text = DataSource[indexPath.row].NewsID
-        
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "asdf"
         return cell
     }
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        performSegueWithIdentifier("cellSegue", sender: self)
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        show(UIViewController(), sender: nil)
     }
     
 }
+
+
+
